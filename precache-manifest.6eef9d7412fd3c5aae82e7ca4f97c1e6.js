d16fd91e1c97b71c559963fa0050fba9ae565f97
Ngo Hoai Than <than1089@gmail.com>
2020-08-08 01:00:51 +0700

    Updates

---
+++ precache-manifest.6eef9d7412fd3c5aae82e7ca4f97c1e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8b8fd5cf2e0e5584f74ebb59bbef807",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a72f977ce60ce85cd826",
+    "url": "/static/css/main.230fe6aa.chunk.css"
+  },
+  {
+    "revision": "630e98197e6b1199ba7b",
+    "url": "/static/js/2.b89eeb11.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b89eeb11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a72f977ce60ce85cd826",
+    "url": "/static/js/main.655ed826.chunk.js"
+  },
+  {
+    "revision": "f9bdc14f60da3addc895",
+    "url": "/static/js/runtime-main.b9197c0d.js"
+  }
+]);
